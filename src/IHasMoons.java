986298cada3d83ab367d56b9092f32f51556e1ad
karan sharma <karan.sharma516@yahoo.com>
2015-02-13 11:38:36 -0800

    Added two interfaces to check whether planet has moons and rings

---
+++ src/IHasMoons.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @author karan sharma
+ * @Description Added the interface to check whether planet has moons or not
+ * @version 1.1
+ */
+public interface IHasMoons {
+        Boolean HasMoons();
+}
---
+++ src/IHasRings.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @author karan sharma
+ * @description added the interface to check whether planet has ring
+ * @version: 1.1
+ */
+public interface IHasRings {
+        Boolean HasRings();
+}
