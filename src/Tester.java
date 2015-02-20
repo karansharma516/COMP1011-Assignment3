7943c50b88468689b104c31b1ac362037f293759
karan sharma <karan.sharma516@yahoo.com>
2015-02-19 19:37:27 -0800

    Finish the project and added the waitForAnyKey method

--- src/Tester.java
+++ src/Tester.java
@@ -1,18 +1,35 @@
 /**
- * @author karan sharma
+ *@author karan sharma
  *@description: this is the tester class which tests the Hero GiantPlanet and TerrestrialPlanet class
  *@version: 1.5
  */
+
+import java.util.Scanner;
+
 public class Tester {
 
 	public static void main(String[] args) {
+		// call the the waitForAnyKey here
+		waitForAnyKey();
 		
-		GiantPlanet earth = new GiantPlanet("Earth", 56, 500, "Gas");
-		System.out.println(earth.toString());
+		GiantPlanet giantPlanet = new GiantPlanet("Earth", 56, 500, "Gas");
+		System.out.println(giantPlanet.toString());
+	
+		System.out.println("++++++++++++++++++++++++++++++++++");
 		
-		TerrestrialPlanet mars = new TerrestrialPlanet("Mars", 57, 800, true);
-		System.out.println(mars.toString());
+		TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Mars", 57, 800, true);
+		System.out.println(terrestrialPlanet.toString());
 		
+		}
+	
+	//This method ask user to press the key to continue
+	public static void waitForAnyKey() {
+		String key;
+		
+	    System.out.println("Please Enter Any Key To Continue");
+	    Scanner input=new Scanner(System.in);
+	    key = input.next();
+	    
 		}
 
 }
